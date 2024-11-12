d233f50b97994eb87c1e1107adafaa304ee15bc7
Midra <me@midra.me>
2024-11-12 18:20:21 +0900

    base64に

--- src/components/SettingsInput/ColorPicker.tsx
+++ src/components/SettingsInput/ColorPicker.tsx
@@ -26,7 +26,7 @@ const PRESET_COLORS = [
 ]
 
 const TRANSPARENT_BACKGROUND_IMAGE =
-  'data:image/svg+xml;utf8,%3Csvg%20width%3D%222%22%20height%3D%222%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%3Cpath%20d%3D%22M0%200h1v2h1V1H0%22%20fill-rule%3D%22nonzero%22%20fill%3D%22%23e1e1e1%22/%3E%3C/svg%3E'
+  'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMiAyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGQ9Ik0wIDBIMVYxSDBWMFoiIGZpbGw9IiNFMUUxRTEiLz48cGF0aCBkPSJNMSAxSDJWMkgxVjFaIiBmaWxsPSIjRTFFMUUxIi8+PC9zdmc+'
 
 type ColorPickerProps = {
   hex: string
@@ -65,14 +65,16 @@ const ColorPicker: React.FC<ColorPickerProps> = (props) => {
             'cursor-pointer'
           )}
         >
-          <div
-            className="absolute inset-0"
-            style={{
-              backgroundImage: `url('${TRANSPARENT_BACKGROUND_IMAGE}')`,
-              backgroundSize: 'auto 50%',
-              backgroundColor: 'rgb(255, 255, 255)',
-            }}
-          />
+          {props.alpha && (
+            <div
+              className="absolute inset-0"
+              style={{
+                backgroundImage: `url('${TRANSPARENT_BACKGROUND_IMAGE}')`,
+                backgroundSize: 'auto 50%',
+                backgroundColor: 'rgb(255, 255, 255)',
+              }}
+            />
+          )}
           <div
             className="absolute inset-0"
             style={{
